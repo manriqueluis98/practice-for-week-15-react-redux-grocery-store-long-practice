baebb6e9bbf38c440596c1a7d165a3bae3da0bc7
Luis <manriqueleo98@gmail.com>
2023-07-12 14:58:02 -0500

    Phase 1 completed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
+
+const store = configureStore()
+
+if(process.env.NODE_ENV !== 'production'){
+  window.store = store
+}
 
 function Root() {
   return (
@@ -13,8 +22,11 @@ function Root() {
 }
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
     <Root />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
+  
   document.getElementById('root')
 );
